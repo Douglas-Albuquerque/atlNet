37c5e1ea6a961074a5a58793916f45c48f2eba36
unknown <douglasalbuquerq@gmail.com>
2024-01-04 08:56:07 -0300

    implementando auto push no servidor.

--- src/Pages/CreateEquip/CreateEquip.js
+++ src/Pages/CreateEquip/CreateEquip.js
@@ -1,9 +1,50 @@
 import React from "react";
 
-function CreateEquip(){
-    return(
-        <h1>Hello World</h1>
+function CreateEquip() {
+    return (
+        <h1>Somehow I know
+            That things are gonna change
+            New boundaries on the way
+            Like never were before
+            Find a meaning to your life
+            Healing whispers of the angels
+            Bring the sunrise again
+
+            New day shines, fallen angels will arise
+            Nova Era brings the ashes back to life
+            (Bring ashes back to life)
+            All over now, all the pain and awful lies
+            Angels will arise back to life
+
+            Fresh winds of hope
+            Has taken us ahead
+            Forever is a place you have to keep in mind
+            No way to limit our goals
+            We'll find a reason to believe
+            Facing Nova Era
+
+            New day shines, fallen angels will arise
+            Nova Era brings the ashes back to life
+            (Bring ashes back to life)
+            All over now, all the pain and awful lies
+            Angels will arise back to life
+
+            Come on, arise
+
+            Find a meaning to your life
+            No way to limit our goals
+            Healing whispers of the angels bring the sunrise again
+
+            New day shines, fallen angels will arise
+            Nova Era brings the ashes back to life (all over now)
+            All over now, sores in pain and awful lies
+            Angels lead you through paradise
+
+            Nova Era is the passage to your mind
+            And forever you may find a better time
+            Nova Era brings the angels back to life
+            Life!</h1>
     )
 };
-    
+
 export default CreateEquip;
