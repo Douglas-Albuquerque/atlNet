d784068be4b18ca0422382a71b59b22ac02f100e
unknown <douglasalbuquerq@gmail.com>
2024-01-17 11:25:02 -0300

    Alteração de Título do Layout

--- src/Components/Layout/LayoutAtl.js
+++ src/Components/Layout/LayoutAtl.js
@@ -53,7 +53,7 @@ const LayoutAtl = () => {
                         />
                     </div>
                     <Menu
-                        style={{ backgroundColor: '#1F4070', color: '#FFF' }}
+                        style={{ backgroundColor: '#FF007F', color: '#FFF' }}
                         theme="dark"
                         color='white'
                         mode="inline"
@@ -97,7 +97,7 @@ const LayoutAtl = () => {
                 <Header
                     style={{
                         padding: 0,
-                        background: "#1F4070",
+                        background: "#FF007F",
                         display: "flex",
                     }}
                 >
